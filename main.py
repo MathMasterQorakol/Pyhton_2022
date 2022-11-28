2fe1ee02b8dd4b66069fc3f7be3177d6d0faf1f0
MathMasterQorakol <119294880+MathMasterQorakol@users.noreply.github.com>
2022-11-28 15:03:59 +0500

    added new file

---
+++ main.py
@@ -0,0 +1,5 @@
+# Muallif : Rahimjon Amonov
+# Sana : 27 - 09 - 2022
+# Maqsad : Pyhton dasturlash tilini o'rganish
+# Ota onamiz biz uchun tinimsiz harakatda mehnat qilishyapti . Sen esa dars qilmay yuribsan
+
